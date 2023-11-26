3d856c55a8129a7715c6ab60b446cb79f5671bcd
root <tosinezekiel03@gmail.com>
2023-11-26 00:41:50 +0000

    DBStorage - Place

--- models/place.py
+++ models/place.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, Table
 from models.review import Review
 import os
@@ -10,9 +10,9 @@ from sqlalchemy.orm import relationship
 class Place(BaseModel, Base):
     """ A place to stay """
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        city_id = Column(String(60), FOreignKey("cities.id"),
+        city_id = Column(String(60), ForeignKey("cities.id"),
                          nullable=False)
-        user_id = Column(String(60), ForignKey("users.id"),
+        user_id = Column(String(60), ForeignKey("users.id"),
                          nullable=False)
         name = Column(String(1024), nullable=False)
         description = Column(String(1024), nullable=False)
